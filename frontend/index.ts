79eec9eacaf2c0ed973e6095f8b4beaacf42febf
Marco Collovati <mcollovati@gmail.com>
2023-06-26 17:56:29 +0200

    fix: initialize the lumo styling
    Thanks to @marcushellberg for the tip.

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,5 +1,11 @@
 import { Router } from '@vaadin/router';
 import './todo-view'
+import { color, typography } from "@vaadin/vaadin-lumo-styles/all-imports.js";
+
+const style = document.createElement("style");
+style.innerHTML = `${color.toString()} ${typography.toString()}`;
+
+document.head.appendChild(style);
 
 export const router = new Router(document.querySelector('#outlet'));
 const routes = [
