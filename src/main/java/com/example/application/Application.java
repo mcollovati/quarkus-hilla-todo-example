d3fb55e57eaa52b3fd70a97723ad56b1cc10466e
Marco Collovati <mcollovati@gmail.com>
2026-01-02 15:47:46 +0100

    chore: bump Vaadin and Quarkus-Hilla to 25.0

---
+++ src/main/java/com/example/application/Application.java
@@ -0,0 +1,9 @@
+package com.example.application;
+
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.lumo.Lumo;
+
+@StyleSheet(Lumo.STYLESHEET)
+public class Application implements AppShellConfigurator {
+}
